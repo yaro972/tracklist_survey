f42e171869f37ded280dd52fc62c9e347ff44017
Thierry Aronoff <thierry.aronoff@gmail.com>
2018-10-31 22:13:56 +0100

    Add new playlist. Remove older playlist

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,98 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-// import logo from './logo.svg';
 import '../styles/App.css';
 import { getPlaylist } from '../actions/index';
 import Track from './track';
 
-// const API_TRACKS = [
-//   {
-//     "id": 1,
-//     "name": "Beast Of Burden",
-//     "duration": 211,
-//     "priority": false,
-//     "artist": "The Rolling Stones",
-//     "adder": {
-//       "id": 1,
-//       "name": "kant",
-//       "pictureUrl": "https://res.cloudinary.com/jukeo-net/image/upload/ano-b2_eezggd"
-//     },
-//     "pictureUrl": "https://i.scdn.co/image/adcf79a6adae4e1a44d0e34860cc103e28ee4372",
-//     "votes": {
-//       "count": 0,
-//       "userVoted": false
-//     }
-//   },
-//   {
-//     "id": 2,
-//     "name": "Purple Haze",
-//     "duration": 229,
-//     "priority": false,
-//     "artist": "Jimi Hendrix",
-//     "adder": {
-//       "id": 1,
-//       "name": "kant",
-//       "pictureUrl": "https://res.cloudinary.com/jukeo-net/image/upload/ano-b2_eezggd"
-//     },
-//     "pictureUrl": "https://i.scdn.co/image/1d55f445789ed89a1b8fab09c3dad117afb5fe80",
-//     "votes": {
-//       "count": 0,
-//       "userVoted": false
-//     }
-//   },
-//   {
-//     "id": 5,
-//     "name": "Nobody my body",
-//     "duration": 229,
-//     "priority": true,
-//     "artist": "Jimi Hendrix",
-//     "adder": {
-//       "id": 1,
-//       "name": "kant",
-//       "pictureUrl": "https://res.cloudinary.com/jukeo-net/image/upload/ano-b2_eezggd"
-//     },
-//     "pictureUrl": "https://i.scdn.co/image/1d55f445789ed89a1b8fab09c3dad117afb5fe80",
-//     "votes": {
-//       "count": 0,
-//       "userVoted": false
-//     }
-//   },
-//   {
-//     "id": 12,
-//     "name": "Bleeding me",
-//     "duration": 229,
-//     "priority": false,
-//     "artist": "Metallica",
-//     "adder": {
-//       "id": 1,
-//       "name": "kant",
-//       "pictureUrl": "https://res.cloudinary.com/jukeo-net/image/upload/ano-b2_eezggd"
-//     },
-//     "pictureUrl": "https://i.scdn.co/image/1d55f445789ed89a1b8fab09c3dad117afb5fe80",
-//     "votes": {
-//       "count": 1,
-//       "userVoted": false
-//     }
-//   },
-//   {
-//     "id": 22,
-//     "name": "Rock your body",
-//     "duration": 229,
-//     "priority": false,
-//     "artist": "Justin Timberlake",
-//     "adder": {
-//       "id": 1,
-//       "name": "kant",
-//       "pictureUrl": "https://res.cloudinary.com/jukeo-net/image/upload/ano-b2_eezggd"
-//     },
-//     "pictureUrl": "https://i.scdn.co/image/1d55f445789ed89a1b8fab09c3dad117afb5fe80",
-//     "votes": {
-//       "count": 1,
-//       "userVoted": false
-//     }
-//   }
-// ];
 
 class App extends Component {
   constructor(props) {
