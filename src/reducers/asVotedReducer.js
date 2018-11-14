4cf058e58fe030dd3bb8f1a20972117eb5ec5c2f
Thierry Aronoff <thierry.aronoff@gmail.com>
2018-11-14 21:57:35 +0100

    Refactor reducer

--- src/reducers/asVotedReducer.js
+++ src/reducers/asVotedReducer.js
@@ -2,6 +2,7 @@
  * Action for voted event
  */
 import { TRACK_VOTED } from '../actions/action-type';
+import {Voted} from '../modules/voted';
 
 const initialState = {};
 
@@ -13,17 +14,7 @@ const initialState = {};
 function AsVotedReducer(state = initialState, action) {
   switch (action.type) {
     case TRACK_VOTED:
-      // let pList = action.payload.track.map((track) => { return track });
-      return action.playlist.map(track => {
-        if (track.id === action.id) {
-          track.votes.count = track.votes.count + 1;
-          track.votes.userVoted = !track.votes.userVoted;
-          return track;
-        } else {
-          return track;
-        }
-      });
-
+      return Voted(action.playlist, action.id);
 
     default:
       return state
