a24ac787ceb5191f87bda911d6fc0ab84218e39d
Thierry Aronoff <thierry.aronoff@gmail.com>
2018-11-15 21:47:03 +0100

    Add new test files

---
+++ src/containers/trackDetail.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import TrackDetail from './trackDetail';
+
+test('Tract detail element', () => {
+  let track = {
+    name: "music track name",
+    duration: 200,
+    artist: 'His name'
+  };
+
+  const component = renderer.create(
+    <TrackDetail track={track} />,
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+});
